e3f7fdd39ff1e1ea4e3b6a3be089bcb6728d2528
YoYoLikesCiCi <YoYoLikesCiCi@gmail.com>
2019-09-12 08:09:27 +0800

    Homework of week 2 c1

---
+++ randomin100.java
@@ -0,0 +1,26 @@
+import java.lang.Math;
+class randomin100{
+public static void main(String[] args){
+	int [] randomnumber = new int[1000];
+    int [] 	save = new int[100];
+	int i=0, temp;
+	for(;i<1000;i++)
+	{
+		//temp
+		temp = (int)(Math.random()*100);
+		randomnumber[i] = temp;
+		save[temp]++;
+	}
+	temp = 0;
+	for(i=0;i<100;i++)
+	{
+	    System.out.print(i+1);
+		System.out.print("---");
+		temp += save[i];
+			System.out.print(save[i]);
+			System.out.print("--temp--");
+			System.out.println(temp);
+	}
+    
+    }
+}
