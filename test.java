903f70491d55f1c1d03b1fec6e50b6e0c5f3e6d3
YoYoLikesCiCi <YoYoLikesCiCi@gmail.com>
2019-09-15 15:13:42 +0800

    nonono

--- test.java
+++ test.java
@@ -1,19 +1,7 @@
-import java.io.IOException;
 class test{
-	public static void main(String[] args) throws IOException
-
-		{
-			int  i, n ;
-		    double	sum =1;
-			n = System.in.read();
-			n -= 48;
-			for (i = 1;  i<= n; i++)
-			{
-				sum *= i;
-			}
-			System.out.println(n+"!=" + sum);
-		}
-
-		
+	public static void print(int x){
+		System.out.println(x);
 	}
 
+	public static void main(String args[]
+}
